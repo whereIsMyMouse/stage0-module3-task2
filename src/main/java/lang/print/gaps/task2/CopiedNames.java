8cc35e2800492762ad81301e9819268122688812
shariy <sharii.margarita@gmail.com>
2022-08-10 17:55:13 +0300

    commit1

--- src/main/java/lang/print/gaps/task2/CopiedNames.java
+++ src/main/java/lang/print/gaps/task2/CopiedNames.java
@@ -2,13 +2,13 @@ package lang.print.gaps.task2;
 
 public class CopiedNames {
     public static void main(String[] args) {
-        int variable = 0;
-        int variable = 2000;
-        int value = 1000;
-        int variable = 4000;
-        int value2 = 3000;
-        int variables = 5000;
-        variables = 7000;
+        int var1 = 0;
+        int var2 = 2000;
+        int var3 = 1000;
+        int var4 = 4000;
+        int var5 = 3000;
+        int var6 = 5000;
+        var6 = 7000;
         int value = 90000;
     }
 }
